bd93d59bfbacd29bf4e376d25a4d01ce4e0808a9
FriesW <william.james.fries@gmail.com>
2019-01-31 11:24:59 -0600

    Experimental Serial UI

--- Teensy/Teensy.ino
+++ Teensy/Teensy.ino
@@ -94,6 +94,7 @@ void setup(){
 
 void loop(){
     databurn();
+    serial_ui();
     MetroManager.update();
 }
 
@@ -128,13 +129,52 @@ void read_ribbon(uint* out_l, uint* out_h){
     
     *out_l = adc.analogRead(pin_l) >> (adc_sample_res - adc_actual_res);
     *out_h = adc.analogRead(pin_h) >> (adc_sample_res - adc_actual_res);
-    Serial.print(*out_l);
+    //Serial.print(*out_l);
+    ////Serial.print(" ");
+    ////Serial.print(*out_h);
+    //uint old = readings.push(*out_l);
     //Serial.print(" ");
-    //Serial.print(*out_h);
-    uint old = readings.push(*out_l);
-    Serial.print(" ");
-    Serial.print(old);
-    Serial.println(" 0 4500");
+    //Serial.print(old);
+    //Serial.println(" 0 4500");
+}
+
+void serial_ui(){
+    static boolean last_dtr;
+    
+    if(!last_dtr && Serial.dtr()){
+        drop();
+        Serial.println(F("***** Ribbon Controller Settings *****"));
+        Serial.println(F("1 : ribbon length calibration"));
+        Serial.println(F("2 : quantized scale selection"));
+        Serial.println(F("3 : quantized scale range"));
+    }
+    last_dtr = Serial.dtr();
+    
+    if(Serial.available() < 2)
+        return;
+    
+    char c = Serial.read();
+    char n = Serial.peek();
+    if(n != '\n' && n != '\r')
+        return;
+    Serial.read();
+    
+    if(c == '1')
+        Serial.println(F("Calibrating ribbon length..."));
+    if(c == '2')
+        Serial.println(F("Input new quantization scale..."));
+    if(c == '3')
+        Serial.println(F("Input new quantized scale range..."));
+    
+    if(c >= '1' && c <= '3'){
+        Serial.println();
+        last_dtr = false;
+    }
+    
+}
+
+void drop(){
+    while(-1 != Serial.read());
 }
 
 void alive(){
