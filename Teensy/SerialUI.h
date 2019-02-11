55ba0eefdae96d34ccd563b2ae72b76b2a74fdba
FriesW <william.james.fries@gmail.com>
2019-02-11 17:40:46 -0600

    Implement other SerialUI inputs

--- Teensy/SerialUI.h
+++ Teensy/SerialUI.h
@@ -16,6 +16,24 @@ class SerialUIClass{
             while(-1 != Serial.read());
         }
         
+        void wait(){
+            while(!Serial.available());
+        }
+        
+        unsigned int parseInt(){
+            wait();
+            while(Serial.peek() < '0' || Serial.peek() > '9'){
+                Serial.read();
+                wait();
+            }
+            unsigned int acc = 0;
+            while(Serial.peek() >= '0' && Serial.peek() <= '9'){
+                acc = (acc*10) + (Serial.read() - '0');
+                wait();
+            }
+            return acc;
+        }
+        
         void opt_1(){
             unsigned int old_v;
             unsigned int new_v;
@@ -33,6 +51,86 @@ class SerialUIClass{
             Serial.print( old_v );
             Serial.println(F(" ohms."));
         }
+        
+        void opt_2(){
+            Serial.println(F("Input new quantization scale."));
+            Serial.println(F("Input 12 characters, representing the twelve chromatic scale notes, starting at 'C'."));
+            Serial.println(F(" In: [a-z|A-Z|#^] -> A letter or '#' or '^' will include that note's position in the scale."));
+            Serial.println(F("Out: [_-*,./\\?' ] -> Punctuation or a space will exclude that note's position in the scale."));
+            Serial.println(F("Template chromatic scale: C^D^EF^G^A^B"));
+            Serial.println(F("        Template C scale: C_D_EF_G_A_B"));
+            Serial.println(F("Input new scale:"));
+            
+            bool notes[12];
+            for(byte i = 0; i < 12; i++){
+                wait();
+                byte v = Serial.read();
+                if( (v>='a' && v<='z') || (v>='A' && v<='Z') || v=='#' || v=='^' )
+                    notes[i] = true;
+                else if ( v=='_' || v=='-' || v=='*' || v==',' || v=='.' || v=='/' || v=='\\' || v=='?' || v=='\'' || v==' ' )
+                    notes[i] = false;
+                else if (v=='r' || v=='\n'){
+                    i--;
+                }
+                else{
+                    Serial.print(F("Invalid: input character '"));
+                    Serial.print((char)v);
+                    Serial.print(F("' at position "));
+                    Serial.print(i);
+                    Serial.println(F(" not recognized."));
+                    return;
+                }
+            }
+            
+            bool valid = false;
+            for(byte i = 0; i < 12; i++)
+                valid = valid || notes[i];
+            if(!valid){
+                Serial.println("Invalid: At least one note must be turned on.");
+                return;
+            }
+            
+            Serial.print("New scale: ");
+            const char lut[] = "C^D^EF^G^A^B";
+            for(byte i = 0; i < 12; i++){
+                if(notes[i])
+                    Serial.print(lut[i]);
+                else
+                    Serial.print('_');
+            }
+            Serial.println();
+            
+        }
+        
+        void opt_3(){
+            Serial.println(F("Input new quantized scale range."));
+            Serial.println(F("Input two midi note numbers [0,127]:"));
+            
+            unsigned int n1 = parseInt();
+            if(n1 > 127){
+                Serial.println(F("Invalid: number out of range."));
+                return;
+            }
+            unsigned int n2 = parseInt();
+            if(n2 > 127){
+                Serial.println(F("Invalid: number out of range."));
+                return;
+            }
+            
+            if(n1 == n2){
+                Serial.println(F("Invalid: numbers must define a range."));
+                return;
+            }
+            
+            unsigned int mi = min(n1, n2);
+            unsigned int ma = max(n1, n2);
+            
+            Serial.print(F("New range: ["));
+            Serial.print(mi);
+            Serial.print(F(", "));
+            Serial.print(ma);
+            Serial.println(F("]"));
+        }
     
     public:
     
@@ -44,34 +142,27 @@ class SerialUIClass{
                 Serial.println(F("1 : ribbon length calibration"));
                 Serial.println(F("2 : quantized scale selection"));
                 Serial.println(F("3 : quantized scale range"));
+                Serial.print(F("\r\n\r\n"));
             }
             last_dtr = Serial.dtr();
             
-            if(Serial.available() < 2)
+            if(!Serial.available())
                 return;
-            
             char c = Serial.read();
-            char n = Serial.peek();
-            if(n != '\n' && n != '\r')
+            if(c < '1' || c > '3'){
+                last_dtr = false;
                 return;
-            Serial.read();
+            }
             
-            bool valid = c >= '1' && c <= '3';
-            
-            if(valid)
-                Serial.println();
+            delay(5); //wait for all incomming chars
+            drop();
             
             if(c == '1') opt_1();
-            if(c == '2')
-                Serial.println(F("Input new quantization scale..."));
-            if(c == '3')
-                Serial.println(F("Input new quantized scale range..."));
+            if(c == '2') opt_2();
+            if(c == '3') opt_3();
             
-            if(valid){
-                Serial.println();
-                last_dtr = false;
-            }
-                    
+            Serial.println();
+            drop();
         };
     
     
